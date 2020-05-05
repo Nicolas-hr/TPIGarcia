f0d0e5849ebbc812ad73a1c1562f89fccd7aa36a
Nocxxecru <nicolas.hr@eduge.ch>
2020-05-05 15:50:52 +0200

    connexion à la bdd

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// Le Type de la base de donnée
+define ('EDB_DBTYPE', "mysql");
+// Connexion local
+define('EDB_HOST', 'localhost');
+define('EDB_DBNAME', 'carnetadresse');
+define('EDB_USER', 'user');
+define('EDB_PASS', 'pwd');
+define('EDB_PORT', 3306);
