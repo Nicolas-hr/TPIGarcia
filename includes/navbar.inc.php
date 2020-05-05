40fb461a5bc5dbb740940b099e5aff1bcce54e67
Nocxxecru <nicolas.hr@eduge.ch>
2020-05-05 15:38:09 +0200

    Création de la navbar

---
+++ includes/navbar.inc.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+    aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./index.php">Accueil</a>
+      </li>
+      <?php if (IsLogged() == false) : ?>
+      <li class="nav-item">
+        <a class="nav-link" href="./register.php">Inscription</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./login.php">Connexion</a>
+      </li>
+      <?php else : ?>
+      <li class="nav-item">
+        <a class="nav-link" href="./logout.php">Déconnexion</a>
+      </li>
+        <?php endif; ?>
+    </ul>
+  </div>
+</nav>
